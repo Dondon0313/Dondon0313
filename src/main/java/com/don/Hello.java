13ad45f136a1dac72decda009b16b3bb3531a3a3
LEEDONGSHENG <1234eggs@gmail.com>
2022-02-17 16:33:09 +0800

    Initial commit

---
+++ src/main/java/com/don/Hello.java
@@ -0,0 +1,14 @@
+package com.don;
+
+public class Hello {
+    public static void main(String[] args) {
+//        System.out.println("Hello world");
+//        new Person().hello();
+        Person p =new Person();
+        p.hello();
+
+
+
+
+    }
+}
