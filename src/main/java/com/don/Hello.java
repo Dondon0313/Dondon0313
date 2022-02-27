dd0d81686765d446b91de33e07591c5378fffa4c
LEEDONGSHENG <1234eggs@gmail.com>
2022-02-27 14:28:59 +0800

    hi.1

--- src/main/java/com/don/Hello.java
+++ src/main/java/com/don/Hello.java
@@ -3,9 +3,13 @@ package com.don;
 public class Hello {
     public static void main(String[] args) {
 //        System.out.println("Hello world");
-//        new Person().hello();
+        new Person().hello();
         Person p =new Person();
         p.hello();
+        char c = '我';
+
+
+
 
 
 
